3aa2b26449c25e67c6c5a0a56934c12a495fb4d0
David Lanner <dhlanner2@gmail.com>
2015-10-26 00:59:11 -0400

    fixed printing multiple results

--- 237/challenge.c
+++ 237/challenge.c
@@ -30,25 +30,6 @@ get_working_keys(char* working_keys_buffer)
   printf("Your working keys are: %s\n", working_keys_buffer);
 }
 
-// void
-// get_intersection(char* string1, char* string2, char* intersecting_chars_buffer)
-// {
-//   memset(intersecting_chars_buffer, 0, sizeof(intersecting_chars_buffer));
-//   int i, j;
-//   int num_intersecting_chars = 0;
-//   for (i = 0; i < strlen(string1); i++)
-//   {
-//     for (j = 0; j < strlen(string2); j++)
-//     {
-//       if (string1[i] == string2[j])
-//       {
-//         intersecting_chars_buffer[num_intersecting_chars] = string1[i];
-//         num_intersecting_chars++;
-//       }
-//     }
-//   }
-// }
-
 bool
 word_is_typeable(char* word, char* working_keys)
 {
@@ -63,8 +44,6 @@ word_is_typeable(char* word, char* working_keys)
       }
     }
   }
-  printf("strlen(%s): %d\n", word, strlen(word));
-  printf("match_count: %d\n", match_count);
   return match_count == strlen(word);
 }
 
@@ -80,18 +59,16 @@ main(int argc, char** argv)
   char valid_word[max_word_size];
   char longest_word[max_word_size];
   memset(longest_word, 0, max_word_size);
-  printf("Longest word so far is: %s (%d long)\n", longest_word, strlen(longest_word));
 
   FILE * fp;
   fp = fopen("/usr/share/dict/words", "r");
-
-  for (i = 0; i < num_lines_to_read; i++)
+  if (fp == NULL)
   {
-    if (fp == NULL)
-    {
-      perror("Error opening file");
-    }
-    else
+    perror("Error opening file");
+  }
+  else
+  {
+    for (i = 0; i < num_lines_to_read; i++)
     {
       rewind(fp);
 
@@ -100,19 +77,17 @@ main(int argc, char** argv)
       while(fgets(valid_word, max_word_size, fp))
       {
         valid_word[strlen(valid_word)-1] = '\0';
-        printf("Valid word: %s (%d long)\n", valid_word, strlen(valid_word));
         if (word_is_typeable(valid_word, working_keys))
         {
-          printf("Longest word so far is: %s (%d long)\n", longest_word, strlen(longest_word));
           if (strlen(valid_word) > strlen(longest_word))
           {
             memset(longest_word, 0, max_word_size);
             strcpy(longest_word, valid_word);
-            printf("Got new longest word: %s\n", longest_word);
-            printf("Longest word so far is: %s (%d long)\n", longest_word, strlen(longest_word));
           }
         }
       }
+      printf("Longest valid word with your working keys is: %s\n", longest_word);
+      memset(longest_word, 0, max_word_size);
       if (ferror(fp))
       {
         exit(1);
@@ -121,6 +96,5 @@ main(int argc, char** argv)
       {
       }
     }
-    printf("Longest valid word with your working keys is: %s\n", longest_word);
   }
 }
